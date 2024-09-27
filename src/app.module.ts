e90456e9e65c0f4c97f561740e333344b30ed677
Daniel Clemente <dcleme17@gmail.com>
2024-09-28 00:20:26 +0100

    add coverage

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,16 +11,10 @@ import { ConsultasModule } from './usecases/consultas/consultas.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ConsultaEntity } from './infrastructure/entities/consulta.entity';
 import { EventRepository } from './infrastructure/repositories/event/event.repository';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { PubSub } from '@google-cloud/pubsub';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      renderPath: '/coverage',
-      rootPath: join(__dirname.replaceAll('\\src', ''), '..', 'coverage', 'lcov-report'),
-    }),
     TypeOrmModule.forRoot({
       type: 'mongodb',
       name: 'agendamentos',
